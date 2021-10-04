e60f1d17248eaabeea8a2537227041fa9d837151
Md kaiser kamal <shahidullahkaiserfaravi@gmail.com>
2021-10-04 19:15:21 +0600

    updated

--- src/App.js
+++ src/App.js
@@ -2,24 +2,50 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const players = [
+    { name: 'rakib', job: 'cricketer' },
+    { name: 'jakib', job: 'cricketer' },
+    { name: 'makib', job: 'cricketer' },
+    { name: 'lakib', job: 'cricketer' },
+    { name: 'pakib', job: 'cricketer' }
+  ];
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {
+        players.map(player => <Player name={player.name} kaj={player.job}></Player>)
+      }
+
     </div>
   );
 }
+
+function Player(props) {
+  const styleCompo = {
+    backgroundColor: 'red',
+    border: '3px solid green',
+    borderRadius: '20px',
+    padding: '20px',
+    margin: '20px'
+
+
+  };
+  return (
+    <div style={styleCompo}>
+      <h2>Name:{props.name}</h2>
+      <h4>kaj:{props.job}</h4>
+    </div>
+  )
+}
+/* 
+ <ul>
+        {
+          players.map(Player => <li>{Player}</li>)
+        }
+      </ul>
+      <Player name={players[0]} job="cricketer" ></Player>
+      <Player name={players[1]} job="cricketer" ></Player>
+      <Player name={players[2]} job="cricketer" ></Player>
+      <Player name={players[3]} job="cricketer" ></Player>
+*/
 
 export default App;
